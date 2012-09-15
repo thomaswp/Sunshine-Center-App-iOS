bc2191810308996438acd3ed6efa2d5d3db53c0f
Thomas <thomaswprice@msn.com>
2012-09-15 16:27:24 -0400

    Progress...

---
+++ Sunshine/Section.h
@@ -0,0 +1,13 @@
+//
+//  Section.h
+//  Sunshine
+//
+//  Created by Thomason Price on 9/15/12.
+//  Copyright (c) 2012 Sunshine Center or NC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Section : NSObject
+
+@end
---
+++ Sunshine/Section.m
@@ -0,0 +1,13 @@
+//
+//  Section.m
+//  Sunshine
+//
+//  Created by Thomason Price on 9/15/12.
+//  Copyright (c) 2012 Sunshine Center or NC. All rights reserved.
+//
+
+#import "Section.h"
+
+@implementation Section
+
+@end
